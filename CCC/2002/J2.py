e62475d465074a507da0e256ea02843f294acf0c
jerry-cheng5 <j.cheng55555@gmail.com>
2020-05-23 17:36:41 -0400

    Finished 2002 J2

---
+++ CCC/2002/J2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+word = ''
+can_words = []
+
+while word != 'quit!':
+    word = input().strip()
+    if word == 'quit!':
+        break
+    if len(word) > 4 and word[-3:] and word[-3] not in ['a', 'e', 'i', 'o', 'u', 'y'] and word[-2:] == 'or':
+        can_words.append(word[:-1] + 'u' + word[-1])
+    else:
+        can_words.append(word)
+
+print('\n'.join(can_words))

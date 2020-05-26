b2081a5fed0a9a0aba263343c669f4da03047fcd
jerry-cheng5 <j.cheng55555@gmail.com>
2020-05-25 23:59:07 -0400

    Finished 2003 S1

---
+++ CCC/2003/S1.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+move = 999
+pos = 1
+snakes = {54 : 19, 90 : 48, 99 : 77}
+ladders = {9 : 34, 40 : 64, 67 : 86}
+
+while pos < 100 and move != 0:
+    move = int(input().strip())
+    if move == 0:
+        print('You Quit!')
+        break
+    elif pos + move == 100:
+        print('You are now on square 100\nYou Win!')
+        break
+    elif pos + move > 100:
+        print('You are now on square', pos)
+    elif pos + move in snakes.keys():
+        pos = snakes[pos + move]
+        print('You are now on square', pos)
+    elif pos + move in ladders.keys():
+        pos = ladders[pos + move]
+        print('You are now on square', pos)
+    else:
+        pos += move
+        print('You are now on square', pos)

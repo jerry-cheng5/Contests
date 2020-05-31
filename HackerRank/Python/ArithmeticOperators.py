c2edfbc5f5bdfac175d6d930db52043110aebda8
jerry-cheng5 <j.cheng55555@gmail.com>
2020-05-31 19:15:44 -0400

    Finished Arithmetic Operators

---
+++ HackerRank/Python/ArithmeticOperators.py
@@ -0,0 +1,13 @@
+'''
+HackerRank
+Arithmetic Operators
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+Jerry Cheng
+'''
+
+a = int(input())
+b = int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)

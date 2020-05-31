b112ec838acc32335264f7702fa40b7ade576827
jerry-cheng5 <j.cheng55555@gmail.com>
2020-05-31 19:16:03 -0400

    Init Data Structures (HackerRank)

---
+++ HackerRank/Data Structures/Arrays.py
@@ -0,0 +1,11 @@
+'''
+HackerRank
+Arrays - DS
+https://www.hackerrank.com/challenges/arrays-ds/problem
+Jerry Cheng
+'''
+
+n = int(input())
+A = input().strip().split()
+A.reverse()
+print(' '.join(A))

9cfb5baf7e7396dd06cae3bc4c71d382ed619c1f
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-06 20:45:02 -0400

    Initialized Algorithms

---
+++ HackerRank/Algorithms/Warmup/Solve_Me_First.py
@@ -0,0 +1,15 @@
+'''
+HackerRank
+Solve Me First (Algorithms)
+https://www.hackerrank.com/challenges/solve-me-first/problem
+Jerry Cheng
+'''
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)

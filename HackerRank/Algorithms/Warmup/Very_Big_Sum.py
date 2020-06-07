153eacfd998352c1da2948fbf80a84dc3ac8c4df
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-06 20:45:20 -0400

    Finished Very Big Sum

---
+++ HackerRank/Algorithms/Warmup/Very_Big_Sum.py
@@ -0,0 +1,34 @@
+'''
+HackerRank
+A Very Big Sum
+https://www.hackerrank.com/challenges/a-very-big-sum/problem
+Jerry Cheng
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

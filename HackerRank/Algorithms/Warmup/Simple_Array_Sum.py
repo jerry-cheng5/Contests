b2cea0b5323e6a84a08f367e68fb8cfff54ca539
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-06 20:44:45 -0400

    Finished Simple Array Sum

---
+++ HackerRank/Algorithms/Warmup/Simple_Array_Sum.py
@@ -0,0 +1,33 @@
+'''
+HackerRank
+Simple Array Sum
+https://www.hackerrank.com/challenges/simple-array-sum/problem
+Jerry Cheng
+'''
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

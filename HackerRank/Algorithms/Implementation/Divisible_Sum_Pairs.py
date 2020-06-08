c9b5389158ff6942c61b66a295ea0adea430120a
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-08 19:42:29 -0400

    Finished Divisible Sum Pairs

---
+++ HackerRank/Algorithms/Implementation/Divisible_Sum_Pairs.py
@@ -0,0 +1,40 @@
+'''
+HackerRank
+Divisible Sum Pairs
+https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+Jerry Cheng
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if i != j and (ar[i]+ar[j]) % k == 0:
+                count += 1
+    return int(count/2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
